4ab237280ecbe7a40c300a3cb2f27b991fbd8334
hailson_junior <hailson_junior@hst.com.br>
2021-08-25 20:13:17 -0300

    Teste

--- 01-Creating-Hosts/s3.tf
+++ 01-Creating-Hosts/s3.tf
@@ -1,5 +1,5 @@
-resource "aws_s3_bucket" "hjopenStackbucket" {
-  bucket = "hjopenStackbucket"
+resource "aws_s3_bucket" "hjopenstackbucket" {
+  bucket = "hjopenstackbucket"
   acl = "private"
 
   tags = {
