06791c9d75ae2fa3a45643857968a2fd6f487764
hailson_junior <hailson_junior@hst.com.br>
2021-08-25 20:18:51 -0300

    bucket

--- 01-Creating-Hosts/main.tf
+++ 01-Creating-Hosts/main.tf
@@ -10,12 +10,12 @@ terraform {
     # Após criar o bucket s3, descomente o módulo abaixo para criar buckup do state
     # Ajuste o profile
     #
-    backend "s3" {
-        bucket = "OpenStack_Bucket"
-        key = "instances-openstack-state/terraform.tfstate"
-        region = "us-east-1"
-        profile = "Terraform"  
-    }
+    #backend "s3" {
+    #    bucket = "OpenStack_Bucket"
+    #    key = "instances-openstack-state/terraform.tfstate"
+    #    region = "us-east-1"
+    #    profile = "Terraform"  
+    #}
 }
 
 provider "aws" {
