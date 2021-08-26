28ecde555211ec248800f4dd1d8a183bb077be17
hailson_junior <hailson_junior@hst.com.br>
2021-08-26 01:08:41 -0300

    OpenStack project - Creating hosts

--- ubuntu/01-Creating-HostsAWS-opcional/instances/create-instances.tf
+++ ubuntu/01-Creating-HostsAWS-opcional/instances/create-instances.tf
@@ -62,17 +62,9 @@ resource "aws_key_pair" "key_openstack" {
 }
 
 output "private_ip" {
-    value = [aws_instance.controller.private_ip,
-            aws_instance.compute.private_ip,
-            aws_instance.block_storage_1.private_ip,
-            aws_instance.object_storage_1.private_ip,
-            aws_instance.object_storage_2.private_ip]
+    value = [aws_instance.openstack_hosts_medium.private_ip]
 }
 
 output "public_dns_controller" {
-    value = [aws_instance.controller.public_dns,
-            aws_instance.compute.public_dns,
-            aws_instance.block_storage_1.public_dns,
-            aws_instance.object_storage_1.public_dns,
-            aws_instance.object_storage_2.public_dns]
+    value = [aws_instance.openstack_hosts_large.public_dns]
 }
