fd6d5e96b60f091bd3c454c4eb0e3f306815c632
Hailson Junior <79525895+HailsonJunior@users.noreply.github.com>
2021-09-10 19:24:54 -0300

    creating vpc

--- ubuntu/00-Creating-HostsAWS-opcional/instances/create-instances.tf
+++ ubuntu/00-Creating-HostsAWS-opcional/instances/create-instances.tf
@@ -1,69 +1,3 @@
-resource "aws_instance" "openstack_hosts_large" {
-    for_each = {
-        node1 = {
-            name = "Controller"
-            network_interface {
-                network_interface_id = aws_network_interface.controller-network.id
-                device_index         = 0
-            }
-        }
-        node2 = {
-            name = "Compute"
-        }
-    }
-
-    instance_type = "t2.micro"
-    ami = "ami-0cb5f8e033cfa84d2"
-    key_name = "${aws_key_pair.key_openstack.key_name}"
-    security_groups = ["Iac_group"]  
-
-    root_block_device {
-        volume_size = 30
-        volume_type = "gp2"
-        delete_on_termination = true
-    }
-    
-    tags = {
-      Projeto = "OpenStack"
-      Name = "${each.key}: ${lookup(each.value, "name", null)}"
-    }
-}
-
-resource "aws_instance" "openstack_hosts_medium" {
-    for_each = {
-        node3 = {
-            name = "Block Storage Node 1"
-            
-        }
-        node4 = {
-            name = "Object Storage Node 1"
-        }
-        node5 = {
-            name = "Object Storage Node 2"
-        }
-    }
-
-    instance_type = "t2.micro"
-    ami = "ami-0cb5f8e033cfa84d2"
-    key_name = "${aws_key_pair.key_openstack.key_name}"
-    security_groups = ["Iac_group"]  
-
-    root_block_device {
-        volume_size = 30
-        volume_type = "gp2"
-        delete_on_termination = true
-    }
-
-    tags = {
-      Projeto = "OpenStack"
-      Name = "${each.key}: ${lookup(each.value, "name", null)}"
-    }
-}
-resource "aws_key_pair" "key_openstack" {
-    key_name = "key_openstack"
-    public_key = "IaC.pem"
-}
-
 resource "aws_vpc" "openstack-vpc" {
     cidr_block = "10.0.0.0/24"
 
@@ -125,4 +59,103 @@ resource "aws_network_interface" "object2-network" {
     tags = {
       "Name" = "primary_network_interface"
     }
+}
+
+resource "aws_instance" "controller" {
+    instance_type = "t2.micro"
+    ami = "ami-0cb5f8e033cfa84d2"
+    key_name = "${aws_key_pair.key_openstack.key_name}"
+    security_groups = ["Iac_group"]
+
+    network_interface {
+      network_interface_id = aws_network_interface.controller-network.id
+      device_index = 0
+    }
+
+    root_block_device {
+        volume_size = 30
+        volume_type = "gp2"
+        delete_on_termination = true
+    }
+
+    tags = {
+        Projeto = "OpenStack"
+        Name = "controller"
+    }
+}
+
+resource "aws_instance" "compute" {
+    instance_type = "t2.micro"
+    ami = "ami-0cb5f8e033cfa84d2"
+    key_name = "${aws_key_pair.key_openstack.key_name}"
+    security_groups = ["Iac_group"]
+
+    root_block_device {
+        volume_size = 30
+        volume_type = "gp2"
+        delete_on_termination = true
+    }
+
+    tags = {
+        Projeto = "OpenStack"
+        Name = "compute"
+    }
+}
+
+resource "aws_instance" "block1" {
+    instance_type = "t2.micro"
+    ami = "ami-0cb5f8e033cfa84d2"
+    key_name = "${aws_key_pair.key_openstack.key_name}"
+    security_groups = ["Iac_group"]
+
+    root_block_device {
+        volume_size = 30
+        volume_type = "gp2"
+        delete_on_termination = true
+    }
+
+    tags = {
+        Projeto = "OpenStack"
+        Name = "block 1"
+    }
+}
+
+resource "aws_instance" "object1" {
+    instance_type = "t2.micro"
+    ami = "ami-0cb5f8e033cfa84d2"
+    key_name = "${aws_key_pair.key_openstack.key_name}"
+    security_groups = ["Iac_group"]
+
+    root_block_device {
+        volume_size = 30
+        volume_type = "gp2"
+        delete_on_termination = true
+    }
+
+    tags = {
+        Projeto = "OpenStack"
+        Name = "object 1"
+    }
+}
+
+resource "aws_instance" "object2" {
+    instance_type = "t2.micro"
+    ami = "ami-0cb5f8e033cfa84d2"
+    key_name = "${aws_key_pair.key_openstack.key_name}"
+    security_groups = ["Iac_group"]
+
+    root_block_device {
+        volume_size = 30
+        volume_type = "gp2"
+        delete_on_termination = true
+    }
+
+    tags = {
+        Projeto = "OpenStack"
+        Name = "object 2"
+    }
+}
+resource "aws_key_pair" "key_openstack" {
+    key_name = "key_openstack"
+    public_key = "IaC.pem"
 }
