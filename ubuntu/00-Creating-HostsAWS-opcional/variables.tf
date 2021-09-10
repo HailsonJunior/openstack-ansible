d85e95d397c4675b660f9aa19f919b86b4ee8a24
Hailson Junior <79525895+HailsonJunior@users.noreply.github.com>
2021-09-10 19:51:39 -0300

    changing variables

--- ubuntu/00-Creating-HostsAWS-opcional/variables.tf
+++ ubuntu/00-Creating-HostsAWS-opcional/variables.tf
@@ -3,6 +3,24 @@ variable "aws_region" {
     description = "Região do privider AWS"
 }
 
+variable "ami" {
+    type = string
+    description = "AMI da instancia"
+    default = "ami-0cb5f8e033cfa84d2"  
+}
+
+variable "block_size" {
+    type = number
+    description = "Temanho do armazenamento"
+    default = 30
+}
+
+variable "block_type" {
+    type = string
+    description = "Tipo do armazenamento"
+    default = "gp2"
+}
+
 # variable "aws_credential" {
 #   type = string
 #    description = "AWS credentials"
