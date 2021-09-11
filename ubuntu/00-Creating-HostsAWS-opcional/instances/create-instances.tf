86323b2dc87a912c79d909b7c21a1cd7f29e17fc
Hailson Junior <79525895+HailsonJunior@users.noreply.github.com>
2021-09-10 21:00:17 -0300

    Security group correction

--- ubuntu/00-Creating-HostsAWS-opcional/instances/create-instances.tf
+++ ubuntu/00-Creating-HostsAWS-opcional/instances/create-instances.tf
@@ -155,3 +155,20 @@ resource "aws_instance" "object2" {
         Name = "object 2"
     }
 }
+
+resource "aws_security_group" "openstack_group" {
+    name = "openstack_group"
+    vpc_id = aws_vpc.openstack-vpc.id
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
