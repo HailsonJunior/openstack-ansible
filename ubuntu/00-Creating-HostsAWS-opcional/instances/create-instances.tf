a789223bedd82588dbae38a2da08635c70d61555
Hailson Ferreira da Silva Junior <hailson.silva@compasso.com.br>
2021-10-22 16:45:28 -0300

    Remoção for each

--- ubuntu/00-Creating-HostsAWS-opcional/instances/create-instances.tf
+++ ubuntu/00-Creating-HostsAWS-opcional/instances/create-instances.tf
@@ -1,26 +1,12 @@
-resource "aws_instance" "openstack_hosts_xlarge" {
-    for_each = {
-        node1 = {
-            name = "Controller"
-            network_interface {
-                network_interface_id = "${aws_network_interface.network-controller.id}"
-                device_index = 0
-            }
-            
-        }
-        node2 = {
-            name = "Compute"
-            network_interface {
-                network_interface_id = "${aws_network_interface.network-compute.id}"
-                device_index = 0
-            }
-        }
-    }
-
+resource "aws_instance" "controller" {
     instance_type = "t2.xlarge"
     ami = var.ami
     key_name = "${aws_key_pair.my-key.key_name}"
     security_groups = ["Iac_group"]  
+    network_interface {
+        network_interface_id = "${aws_network_interface.network-controller.id}"
+        device_index = 0
+    }
 
     root_block_device {
         volume_size = var.block_size
@@ -30,33 +16,41 @@ resource "aws_instance" "openstack_hosts_xlarge" {
     
     tags = {
       Projeto = "OpenStack"
-      Name = "${each.key}: ${lookup(each.value, "name", null)}"
+      Name = "Controller"
     }
 }
 
-resource "aws_instance" "openstack_hosts_medium" {
-    for_each = {
-        node3 = {
-            name = "Block Storage Node 1"
-            network_interface {
-                network_interface_id = "${aws_network_interface.network-block.id}"
-                device_index = 0
-            }
-            
-        }
-        node4 = {
-            name = "Object Storage Node 1"
-            network_interface {
-                network_interface_id = "${aws_network_interface.network-object.id}"
-                device_index = 0
-            }
-        }
-    }
-
-    instance_type = "t2.micro"
+resource "aws_instance" "compute" {
+    instance_type = "t2.xlarge"
     ami = var.ami
     key_name = "${aws_key_pair.my-key.key_name}"
     security_groups = ["Iac_group"]  
+    network_interface {
+        network_interface_id = "${aws_network_interface.network-compute.id}"
+        device_index = 0
+    }
+
+    root_block_device {
+        volume_size = var.block_size
+        volume_type = var.block_type
+        delete_on_termination = true
+    }
+    
+    tags = {
+      Projeto = "OpenStack"
+      Name = "Compute"
+    }
+}
+
+resource "aws_instance" "block" {
+    instance_type = "t2.micro"
+    ami = var.ami
+    key_name = "${aws_key_pair.my-key.key_name}"
+    security_groups = ["Iac_group"]
+    network_interface {
+        network_interface_id = "${aws_network_interface.network-block.id}"
+        device_index = 0
+    }
 
     root_block_device {
         volume_size = var.block_size
@@ -66,7 +60,29 @@ resource "aws_instance" "openstack_hosts_medium" {
 
     tags = {
       Projeto = "OpenStack"
-      Name = "${each.key}: ${lookup(each.value, "name", null)}"
+      Name = "Block Storage"
+    }
+}
+
+resource "aws_instance" "object" {
+    instance_type = "t2.micro"
+    ami = var.ami
+    key_name = "${aws_key_pair.my-key.key_name}"
+    security_groups = ["Iac_group"]
+    network_interface {
+        network_interface_id = "${aws_network_interface.network-object.id}"
+        device_index = 0
+    }
+
+    root_block_device {
+        volume_size = var.block_size
+        volume_type = var.block_type
+        delete_on_termination = true
+    }
+
+    tags = {
+      Projeto = "OpenStack"
+      Name = "Object Storage"
     }
 }
 
