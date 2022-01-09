bbfbc0153c4aa0b83990bc5f39acc41a70656909
HailsonJunior <hailson.fsj@gmail.com>
2022-01-09 06:02:57 +0000

    Removing s3 modul

--- ubuntu/00-Creating-HostsAWS-opcional/main.tf
+++ ubuntu/00-Creating-HostsAWS-opcional/main.tf
@@ -7,20 +7,10 @@ terraform {
   }
   required_version = ">= 0.14.9"
 
-  #  backend "s3" {
-  #      bucket = "openstackbucket"
-  #      key = "instances-openstack-state/terraform.tfstate"
-  #      region = "us-east-1"
-  #      profile = "Terraform"  
-  #  }
 }
 
 provider "aws" {
   region = var.aws_region
-}
-
-module "s3" {
-  source = "./s3"
 }
 
 module "instances" {
--- ubuntu/00-Creating-HostsAWS-opcional/s3/variables.tf
+++
@@ -1,6 +0,0 @@
-variable "bucket_name" {
-    type = string
-    description = "AWS Bucket Name"
-    default = "openstackbucket"    # Ajuste o nome do bucket, informando um nome valido
-}
-
